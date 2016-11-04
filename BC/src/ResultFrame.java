ab8c0e52c1b74d88ad627266908c72510a165fa0
jesse <jesse@DESKTOP-9MR1L9U>
2016-11-04 23:41:26 +0900

    지혜/공강 찾기 결과화면 수정

--- BC/src/ResultFrame.java
+++ BC/src/ResultFrame.java
@@ -17,6 +17,7 @@ import javax.swing.table.DefaultTableModel;
 import javax.swing.JTable;
 import javax.swing.SwingConstants;
 import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JComponent;
 import java.awt.image.BufferedImage;
@@ -24,6 +25,8 @@ import java.io.File;
 import java.io.IOException;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+import java.awt.Font;
+import javax.swing.JTabbedPane;
 
 public class ResultFrame extends JFrame {
 
@@ -34,6 +37,7 @@ public class ResultFrame extends JFrame {
 	private JPanel contentPane;
 	int[][] ex;
 	private static JTable table_1;
+
 	/**
 	 * Launch the application.
 	 */
@@ -41,26 +45,27 @@ public class ResultFrame extends JFrame {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
-					int[][] a= new int[145][7];
-					for(int i=1;i<145;i++){//배열 입력 부분
-						if((i>=1)&&(i<=15))
-							a[i][1]=1;
-						else if((i>=17)&&(i<=53))
-							a[i][1]=1;
-						else if((i>=55)&&(i<=69))
-							a[i][1]=1;
-						else if((i>=85)&&(i<= 105))
-							a[i][1]=1;
-						if((i>=10)&&(i<=100))
-							a[i][3]=1;					}
-					
-					ResultFrame frame = new ResultFrame();//프레임 생성					
-					frame.setVisible(true);				
-					
+					int[][] a = new int[145][7];
+					for (int i = 1; i < 145; i++) {// 배열 입력 부분
+						if ((i >= 1) && (i <= 15))
+							a[i][1] = 1;
+						else if ((i >= 17) && (i <= 53))
+							a[i][1] = 1;
+						else if ((i >= 55) && (i <= 69))
+							a[i][1] = 1;
+						else if ((i >= 85) && (i <= 105))
+							a[i][1] = 1;
+						if ((i >= 10) && (i <= 100))
+							a[i][3] = 1;
+					}
+
+					ResultFrame frame = new ResultFrame();// 프레임 생성
+					frame.setVisible(true);
+
 					TableCellRenderer renderer = new TableCellRenderer();
-					renderer.getinfo(a);//배열을 render의 배열안에 입력					
+					renderer.getinfo(a);// 배열을 render의 배열안에 입력
 					table_1.setDefaultRenderer(Class.forName("java.lang.Object"), renderer);
-					
+
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
@@ -70,133 +75,189 @@ public class ResultFrame extends JFrame {
 
 	/**
 	 * Create the frame.
-	 * @throws ClassNotFoundException 
+	 * 
+	 * @throws ClassNotFoundException
 	 */
 	public ResultFrame() throws ClassNotFoundException {
 		setTitle("\uBAA8\uB450\uC758 \uACF5\uAC15");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 746, 800);
+		setBounds(100, 100, 972, 934);
 		contentPane = new JPanel();
+		contentPane.setBackground(Color.WHITE);
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
-		
-		Object Rowdata[][]={{"09:00"},{"10:00"},{"11:00"},{"12:00"},{"13:00"},{"14:00"},{"15:00"},{"16:00"},{"17:00"},
-		{"18:00"},{"19:00"},{"20:00"},{"21:00"}};		
-		
-		Object columnNames[] = {"","월","화","수","목","금","토"};
-		DefaultTableModel defaultTableModel1 = new DefaultTableModel(Rowdata,columnNames){
-			/**
-			 * 
-			 */
-			private static final long serialVersionUID = 1L;//느낌표 표시 없애기 위해 작성 신경쓸 필요 없음
 
-			public boolean isCellEditable(int row, int column){//cell 수정 불가 설정
+		Object Rowdata[][] = { { "09:00" }, { "10:00" }, { "11:00" }, { "12:00" }, { "13:00" }, { "14:00" },
+				{ "15:00" }, { "16:00" }, { "17:00" }, { "18:00" }, { "19:00" }, { "20:00" }, { "21:00" } };
+
+		Object columnNames[] = { "", "월", "화", "수", "목", "금", "토" };
+		DefaultTableModel defaultTableModel1 = new DefaultTableModel(Rowdata, columnNames) {
+			private static final long serialVersionUID = 1L;// 느낌표 표시 없애기 위해 작성
+															// 신경쓸 필요 없음
+
+			public boolean isCellEditable(int row, int column) {// cell 수정 불가 설정
 				return false;
-			}	
+			}
 		};
-		
-		contentPane.setLayout(null);			
+
+		contentPane.setLayout(null);
 		table_1 = new JTable(defaultTableModel1);
+		table_1.setFont(new Font("ZESSTYPE 비가온다 PT02", Font.PLAIN, 24));
 		table_1.setBounds(37, 35, 402, 628);
-		table_1.setRowHeight(50);
-		table_1.setIntercellSpacing(new Dimension(1,0));//???? 선여백처리??
+		table_1.setRowHeight(53);
+		table_1.setIntercellSpacing(new Dimension(1, 0));// ???? 선여백처리??
 		contentPane.add(table_1);
-		
+
 		JScrollPane scrollPane = new JScrollPane(table_1);
-		scrollPane.setBounds(37, 39, 504, 675);
+		scrollPane.setBounds(52, 124, 680, 722);
 		contentPane.add(scrollPane);
-		JButton button = new JButton("\uC800\uC7A5");
-		button.setBounds(595, 186, 97, 23);
+		JButton button = new JButton("");
+		button.setBorderPainted(false);    //투명 버튼
+		button.setFocusPainted(false);      //투명 버튼
+		button.setContentAreaFilled(false); //투명 버튼
+		button.setBounds(789, 713, 124, 52);
 		button.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
-				try{
-				    Dimension d = new Dimension(504, 675);//오른쪽 아래 좌표 얻기위해 디멘션 사용
-				    Rectangle area = new Rectangle(scrollPane.getLocationOnScreen(), d.getSize());//getlocationonscreen은 실시간 어플리케이션 scrollPane 위치의 왼쪽 위 지점을 포인트로 얻어옴||
-				    
-				    Robot robot = new Robot();
-				    BufferedImage bufferedImage = robot.createScreenCapture(area);
-				    // 전체 스크린 샷 가져오기
-				    // Capture the whole screen
-				   // area = new Rectangle(Toolkit.getDefaultToolkit().getScreenSize());
-				   // bufferedImage = robot.createScreenCapture(area);
-				    ImageIO.write(bufferedImage,"jpg",new File("C:/Users/HBC/Desktop/screenshot.jpg"));
-				    
-			}catch (AWTException e){
-			}catch (IOException e){
-			}
+				try {
+					Dimension d = new Dimension(504, 675);// 오른쪽 아래 좌표 얻기위해 디멘션
+															// 사용
+					Rectangle area = new Rectangle(scrollPane.getLocationOnScreen(), d.getSize());// getlocationonscreen은
+																									// 실시간
+																									// 어플리케이션
+																									// scrollPane
+																									// 위치의
+																									// 왼쪽
+																									// 위
+																									// 지점을
+																									// 포인트로
+																									// 얻어옴||
+
+					Robot robot = new Robot();
+					BufferedImage bufferedImage = robot.createScreenCapture(area);
+					// 전체 스크린 샷 가져오기
+					// Capture the whole screen
+					// area = new
+					// Rectangle(Toolkit.getDefaultToolkit().getScreenSize());
+					// bufferedImage = robot.createScreenCapture(area);
+					ImageIO.write(bufferedImage, "jpg", new File("C:/Users/HBC/Desktop/screenshot.jpg"));
+
+				} catch (AWTException e) {
+				} catch (IOException e) {
+				}
 			}
 		});
 		contentPane.add(button);
+
+		JPanel panel = new JPanel() {
+			ImageIcon mainicon = new ImageIcon("result.png");
+
+			public void paintComponent(Graphics g) {
+				g.drawImage(mainicon.getImage(), 0, 0, null);
+				setOpaque(false);
+				super.paintComponent(g);
+			}
+		};
+		panel.setBounds(52, 28, 368, 94);
+		contentPane.add(panel);
+		
+		
+		//저장 버튼용 판넬
+		JPanel panel_1 = new JPanel(){
+			ImageIcon mainicon = new ImageIcon("result_save.png");
+
+			public void paintComponent(Graphics g) {
+				g.drawImage(mainicon.getImage(), 0, 0, null);
+				setOpaque(false);
+				super.paintComponent(g);
+			}
+		};
+		panel_1.setBounds(784, 713, 129, 52);
+		contentPane.add(panel_1);
 	}
 }
-class TableCellRenderer extends DefaultTableCellRenderer{//배열 색 채우기
+
+class TableCellRenderer extends DefaultTableCellRenderer {// 배열 색 채우기
 	/**
 	 * 
 	 */
-	TableCellRenderer(){
+	TableCellRenderer() {
 		this.setHorizontalAlignment(SwingConstants.CENTER);
 	};
-	int[][] array; 
-	int row_=0;
-	int col =0;
-	int check=0,first1=0,first2=0,end1=0,end2=0;
-	private static final long serialVersionUID = 1L;//느낌표 표시 없애기 위해 작성 신경쓸 필요 없음
-	public Component getTableCellRendererComponent(JTable table,Object value,boolean isSelected,boolean hasFocus,int row, int column){
-		Component cell = super.getTableCellRendererComponent(table,value,isSelected,hasFocus,row,column);
-		row_=row;
-		col=column;
-		if(!isSelected){			
-			////////////////////////////////			
+
+	int[][] array;
+	int row_ = 0;
+	int col = 0;
+	int check = 0, first1 = 0, first2 = 0, end1 = 0, end2 = 0;
+	private static final long serialVersionUID = 1L;// 느낌표 표시 없애기 위해 작성 신경쓸 필요
+													// 없음
+
+	public Component getTableCellRendererComponent(JTable table, Object value, boolean isSelected, boolean hasFocus,
+			int row, int column) {
+		Component cell = super.getTableCellRendererComponent(table, value, isSelected, hasFocus, row, column);
+		row_ = row;
+		col = column;
+		if (!isSelected) {
+			////////////////////////////////
 		}
-		return cell;	
+		return cell;
 	}
-    public void setUI(LabelUI ui)
-    {
-        super.setUI(new BasicLabelUI()
-        {
-            public void paint(Graphics g, JComponent c)
-            {
-            	Rectangle r = g.getClipBounds();    
-                super.paint(g, c);
-                g.setColor(Color.green);
-               for(int j=1;j<7;j++){
-                 	for(int i=1;i<145;i++){
-                 		if((array[i][j]==1)&&(check==0)){//시간표 시작하는 부분 저장
-                 			check=1;
-                 			first1=i/12;// i에 대한 몫
-                 			first2=(i-1)%12;  //i에 대한 나머지                			
-                 		}
-                 		
-                 		else if((array[i][j]==0)&&(check==1)){//시간표 끝나는 부분 저장
-                 			check=0;
-                 			end1=(i-1)/12;//i에 대한 몫
-                 			end2=(i-1)%12;//i에 대한 나머지                 			
-                 			for(int k=first1;k<=end1;k++){
-                 				if(k==first1){
-                 					if((row_==k)&&(col==j)){                 						
-                           				g.fillRect(r.x, r.y+(r.height/12*first2), r.width, r.height);//시작할때는 출발하는 부분의 높이를 조절
-                           				System.out.println("First2 : "+first2+"\n");
-                 					}                 					
-                 				}
-                 				else if(k==end1){
-                 					if((row_==k)&&(col==j)){                 						
-                           				g.fillRect(r.x, r.y, r.width, r.height/12*end2);//끝날때는 끝나는 부분의 높이를 조절
-                           				System.out.println("End2 : "+end2+"\n");
-                 					}      
-                 				}
-                 				else{
-                 					if((row_==k)&&(col==j)){   //시간이 길 경우 중간 공백 채우는 부분              						
-                           				g.fillRect(r.x, r.y, r.width, r.height);
-                 					}      
-                 				}
-                 			}
-                 		}                 	
-                 	}
-                }              
-            }
-        });
-    }
-	void getinfo(int a[][]){//배열 얻어오는부분
-		this.array= a;
+
+	public void setUI(LabelUI ui) {
+		super.setUI(new BasicLabelUI() {
+			public void paint(Graphics g, JComponent c) {
+				Rectangle r = g.getClipBounds();
+				super.paint(g, c);
+				g.setColor(Color.green);
+				for (int j = 1; j < 7; j++) {
+					for (int i = 1; i < 145; i++) {
+						if ((array[i][j] == 1) && (check == 0)) {// 시간표 시작하는 부분
+																	// 저장
+							check = 1;
+							first1 = i / 12;// i에 대한 몫
+							first2 = (i - 1) % 12; // i에 대한 나머지
+						}
+
+						else if ((array[i][j] == 0) && (check == 1)) {// 시간표 끝나는
+																		// 부분 저장
+							check = 0;
+							end1 = (i - 1) / 12;// i에 대한 몫
+							end2 = (i - 1) % 12;// i에 대한 나머지
+							for (int k = first1; k <= end1; k++) {
+								if (k == first1) {
+									if ((row_ == k) && (col == j)) {
+										g.fillRect(r.x, r.y + (r.height / 12 * first2), r.width, r.height);// 시작할때는
+																											// 출발하는
+																											// 부분의
+																											// 높이를
+																											// 조절
+										System.out.println("First2 : " + first2 + "\n");
+									}
+								} else if (k == end1) {
+									if ((row_ == k) && (col == j)) {
+										g.fillRect(r.x, r.y, r.width, r.height / 12 * end2);// 끝날때는
+																							// 끝나는
+																							// 부분의
+																							// 높이를
+																							// 조절
+										System.out.println("End2 : " + end2 + "\n");
+									}
+								} else {
+									if ((row_ == k) && (col == j)) { // 시간이 길 경우
+																		// 중간 공백
+																		// 채우는
+																		// 부분
+										g.fillRect(r.x, r.y, r.width, r.height);
+									}
+								}
+							}
+						}
+					}
+				}
+			}
+		});
+	}
+
+	void getinfo(int a[][]) {// 배열 얻어오는부분
+		this.array = a;
 	}
 }
