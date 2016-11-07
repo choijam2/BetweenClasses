f626ab4320f70746fbdf3c3492787ab601cf5450
choijam2 <choijam1104@gmail.com>
2016-11-08 00:35:40 +0900

    지혜/공강 시간표 추가

--- BC/src/AddClassFrame.java
+++ BC/src/AddClassFrame.java
@@ -26,11 +26,17 @@ import javax.swing.border.BevelBorder;
 import javax.swing.border.CompoundBorder;
 import javax.swing.border.LineBorder;
 
-public class AddClassFrame extends JFrame {
+public class AddClassFrame2 extends JFrame {
 
 	private JPanel contentPane;
 	private JTable table;
 	private JTextField txt_ClassNum;
+	private JTable table_1;
+	private JTable table_2;
+	private JTable table_3;
+	private JTable table_4;
+	private JTable table_5;
+	private JTable table_6;
 
 	/**
 	 * Launch the application.
@@ -39,7 +45,7 @@ public class AddClassFrame extends JFrame {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
-					AddClassFrame frame = new AddClassFrame();
+					AddClassFrame2 frame = new AddClassFrame2();
 					frame.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
@@ -51,9 +57,9 @@ public class AddClassFrame extends JFrame {
 	/**
 	 * Create the frame.
 	 */
-	public AddClassFrame() {
+	public AddClassFrame2() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 1077, 967);
+		setBounds(100, 100, 1283, 976);
 		contentPane = new JPanel();
 		contentPane.setBackground(Color.WHITE);
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
@@ -62,7 +68,7 @@ public class AddClassFrame extends JFrame {
 
 		
 		JPanel ParentPanel = new JPanel();
-		ParentPanel.setBounds(0, 115, 1055, 796);
+		ParentPanel.setBounds(0, 115, 1261, 796);
 		contentPane.add(ParentPanel);
 		ParentPanel.setLayout(new CardLayout(0, 0));
 		
@@ -112,7 +118,7 @@ public class AddClassFrame extends JFrame {
 				super.paintComponent(g);
 			}
 		};
-		pnl_save.setBounds(884, 368, 105, 47);
+		pnl_save.setBounds(884, 427, 105, 47);
 		
 		JPanel pnl_del = new JPanel(){
 			//판넬에 이미지 추가
@@ -124,7 +130,7 @@ public class AddClassFrame extends JFrame {
 				super.paintComponent(g);
 			}
 		};
-		pnl_del.setBounds(884, 430, 105, 55);
+		pnl_del.setBounds(884, 368, 105, 55);
 		
 				JButton btn_Del = new JButton();
 				pnl_del.add(btn_Del);
@@ -159,8 +165,6 @@ public class AddClassFrame extends JFrame {
 		table.setFont(new Font("ZESSTYPE 비가온다 PT02", Font.PLAIN, 24));
 		table.setBounds(37, 35, 402, 600);
 		table.setRowHeight(53);
-		table.getTableHeader().setReorderingAllowed(false);//column 헤더수정불가
-		table.getTableHeader().setResizingAllowed(false);//column 헤더길이조절불가
 		table.setModel(new DefaultTableModel(
 			new Object[][] {
 				{"9:00", null, null, null, null, null, null},
@@ -180,14 +184,7 @@ public class AddClassFrame extends JFrame {
 			new String[] {
 				"Time", "월", "화", "수", "목", "금", "토"
 			}
-		){				
-			private static final long serialVersionUID = 1L;
-
-				public boolean isCellEditable(int row, int column) {// cell 수정 불가 설정
-					return false;
-				}
-			
-		});
+		));
 		
 		//가운데 정렬
 		table.getColumn("Time").setCellRenderer(celAlignCenter);
@@ -204,8 +201,212 @@ public class AddClassFrame extends JFrame {
 		
 		
 		JPanel panel_2 = new JPanel();
-		panel_2.setBackground(Color.orange);
+		panel_2.setBorder(new LineBorder(new Color(0, 0, 0), 2));
+		panel_2.setBackground(Color.WHITE);
 		ParentPanel.add(panel_2, "name_798285310498133");
+		
+		JScrollPane scrolltime1 = new JScrollPane();
+		
+		JScrollPane scrollPane_2 = new JScrollPane();
+		
+		JScrollPane scrollPane_3 = new JScrollPane();
+		
+		JScrollPane scrollPane_4 = new JScrollPane();
+		
+		JScrollPane scrollPane_5 = new JScrollPane();
+		
+		JScrollPane scrollPane_6 = new JScrollPane();
+		
+		JScrollPane scrollPane_7 = new JScrollPane();
+		GroupLayout gl_panel_2 = new GroupLayout(panel_2);
+		gl_panel_2.setHorizontalGroup(
+			gl_panel_2.createParallelGroup(Alignment.LEADING)
+				.addGroup(gl_panel_2.createSequentialGroup()
+					.addGap(27)
+					.addGroup(gl_panel_2.createParallelGroup(Alignment.LEADING)
+						.addGroup(gl_panel_2.createSequentialGroup()
+							.addComponent(scrolltime1, GroupLayout.PREFERRED_SIZE, 310, GroupLayout.PREFERRED_SIZE)
+							.addGap(18)
+							.addComponent(scrollPane_2, GroupLayout.PREFERRED_SIZE, 310, GroupLayout.PREFERRED_SIZE)
+							.addGap(18)
+							.addComponent(scrollPane_3, GroupLayout.PREFERRED_SIZE, 310, GroupLayout.PREFERRED_SIZE))
+						.addGroup(gl_panel_2.createSequentialGroup()
+							.addPreferredGap(ComponentPlacement.RELATED)
+							.addComponent(scrollPane_4, GroupLayout.PREFERRED_SIZE, 310, GroupLayout.PREFERRED_SIZE)
+							.addGap(17)
+							.addComponent(scrollPane_6, GroupLayout.PREFERRED_SIZE, 310, GroupLayout.PREFERRED_SIZE)
+							.addGap(18)
+							.addComponent(scrollPane_7, GroupLayout.PREFERRED_SIZE, 310, GroupLayout.PREFERRED_SIZE)
+							.addGap(2)
+							.addComponent(scrollPane_5, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)))
+					.addContainerGap(264, Short.MAX_VALUE))
+		);
+		gl_panel_2.setVerticalGroup(
+			gl_panel_2.createParallelGroup(Alignment.TRAILING)
+				.addGroup(gl_panel_2.createSequentialGroup()
+					.addGap(59)
+					.addGroup(gl_panel_2.createParallelGroup(Alignment.LEADING)
+						.addGroup(gl_panel_2.createParallelGroup(Alignment.BASELINE)
+							.addComponent(scrollPane_2, GroupLayout.PREFERRED_SIZE, 270, GroupLayout.PREFERRED_SIZE)
+							.addComponent(scrollPane_3, GroupLayout.PREFERRED_SIZE, 270, GroupLayout.PREFERRED_SIZE))
+						.addComponent(scrolltime1, GroupLayout.PREFERRED_SIZE, 270, GroupLayout.PREFERRED_SIZE))
+					.addGroup(gl_panel_2.createParallelGroup(Alignment.LEADING)
+						.addGroup(gl_panel_2.createSequentialGroup()
+							.addPreferredGap(ComponentPlacement.RELATED, 97, Short.MAX_VALUE)
+							.addComponent(scrollPane_5, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+							.addGap(364))
+						.addGroup(gl_panel_2.createSequentialGroup()
+							.addGap(49)
+							.addGroup(gl_panel_2.createParallelGroup(Alignment.LEADING)
+								.addComponent(scrollPane_4, GroupLayout.PREFERRED_SIZE, 270, GroupLayout.PREFERRED_SIZE)
+								.addGroup(gl_panel_2.createParallelGroup(Alignment.BASELINE)
+									.addComponent(scrollPane_6, GroupLayout.PREFERRED_SIZE, 270, GroupLayout.PREFERRED_SIZE)
+									.addComponent(scrollPane_7, GroupLayout.PREFERRED_SIZE, 270, GroupLayout.PREFERRED_SIZE)))
+							.addContainerGap())))
+		);
+		
+		table_6 = new JTable();
+		table_6.getTableHeader().setFont(new Font("ZESSTYPE 비가온다 PT02", Font.BOLD, 18));
+		table_6.setFont(new Font("ZESSTYPE 비가온다 PT02", Font.PLAIN, 18));
+		table_6.setRowHeight(19);
+		table_6.setModel(new DefaultTableModel(
+				new Object[][] {
+					{"9:00", null, null, null, null, null, null},
+					{"10:00", null, null, null, null, null, null},
+					{"11:00", null, null, null, null, null, null},
+					{"12:00", null, null, null, null, null, null},
+					{"13:00", null, null, null, null, null, null},
+					{"14:00", null, null, null, null, null, null},
+					{"15:00", null, null, null, null, null, null},
+					{"16:00", null, null, null, null, null, null},
+					{"17:00", null, null, null, null, null, null},
+					{"18:00", null, null, null, null, null, null},
+					{"19:00", null, null, null, null, null, null},
+					{"20:00", null, null, null, null, null, null},
+					{"21:00", null, null, null, null, null, null},
+				},
+				new String[] {
+					"Time", "월", "화", "수", "목", "금", "토"
+				}
+		));
+		scrollPane_7.setViewportView(table_6);
+		
+		table_5 = new JTable();
+		table_5.setModel(new DefaultTableModel(
+				new Object[][] {
+					{"9:00", null, null, null, null, null, null},
+					{"10:00", null, null, null, null, null, null},
+					{"11:00", null, null, null, null, null, null},
+					{"12:00", null, null, null, null, null, null},
+					{"13:00", null, null, null, null, null, null},
+					{"14:00", null, null, null, null, null, null},
+					{"15:00", null, null, null, null, null, null},
+					{"16:00", null, null, null, null, null, null},
+					{"17:00", null, null, null, null, null, null},
+					{"18:00", null, null, null, null, null, null},
+					{"19:00", null, null, null, null, null, null},
+					{"20:00", null, null, null, null, null, null},
+					{"21:00", null, null, null, null, null, null},
+				},
+				new String[] {
+					"Time", "월", "화", "수", "목", "금", "토"
+				}
+		));
+		scrollPane_6.setViewportView(table_5);
+		
+		table_4 = new JTable();
+		table_4.setModel(new DefaultTableModel(
+				new Object[][] {
+					{"9:00", null, null, null, null, null, null},
+					{"10:00", null, null, null, null, null, null},
+					{"11:00", null, null, null, null, null, null},
+					{"12:00", null, null, null, null, null, null},
+					{"13:00", null, null, null, null, null, null},
+					{"14:00", null, null, null, null, null, null},
+					{"15:00", null, null, null, null, null, null},
+					{"16:00", null, null, null, null, null, null},
+					{"17:00", null, null, null, null, null, null},
+					{"18:00", null, null, null, null, null, null},
+					{"19:00", null, null, null, null, null, null},
+					{"20:00", null, null, null, null, null, null},
+					{"21:00", null, null, null, null, null, null},
+				},
+				new String[] {
+					"Time", "월", "화", "수", "목", "금", "토"
+				}
+		));
+		scrollPane_4.setViewportView(table_4);
+		
+		table_3 = new JTable();
+		table_3.setModel(new DefaultTableModel(
+				new Object[][] {
+					{"9:00", null, null, null, null, null, null},
+					{"10:00", null, null, null, null, null, null},
+					{"11:00", null, null, null, null, null, null},
+					{"12:00", null, null, null, null, null, null},
+					{"13:00", null, null, null, null, null, null},
+					{"14:00", null, null, null, null, null, null},
+					{"15:00", null, null, null, null, null, null},
+					{"16:00", null, null, null, null, null, null},
+					{"17:00", null, null, null, null, null, null},
+					{"18:00", null, null, null, null, null, null},
+					{"19:00", null, null, null, null, null, null},
+					{"20:00", null, null, null, null, null, null},
+					{"21:00", null, null, null, null, null, null},
+				},
+				new String[] {
+					"Time", "월", "화", "수", "목", "금", "토"
+				}
+		));
+		scrollPane_3.setViewportView(table_3);
+		
+		table_2 = new JTable();
+		table_2.setModel(new DefaultTableModel(
+				new Object[][] {
+					{"9:00", null, null, null, null, null, null},
+					{"10:00", null, null, null, null, null, null},
+					{"11:00", null, null, null, null, null, null},
+					{"12:00", null, null, null, null, null, null},
+					{"13:00", null, null, null, null, null, null},
+					{"14:00", null, null, null, null, null, null},
+					{"15:00", null, null, null, null, null, null},
+					{"16:00", null, null, null, null, null, null},
+					{"17:00", null, null, null, null, null, null},
+					{"18:00", null, null, null, null, null, null},
+					{"19:00", null, null, null, null, null, null},
+					{"20:00", null, null, null, null, null, null},
+					{"21:00", null, null, null, null, null, null},
+				},
+				new String[] {
+					"Time", "월", "화", "수", "목", "금", "토"
+				}
+		));
+		scrollPane_2.setViewportView(table_2);
+		
+		table_1 = new JTable();
+		table_1.setModel(new DefaultTableModel(
+				new Object[][] {
+					{"9:00", null, null, null, null, null, null},
+					{"10:00", null, null, null, null, null, null},
+					{"11:00", null, null, null, null, null, null},
+					{"12:00", null, null, null, null, null, null},
+					{"13:00", null, null, null, null, null, null},
+					{"14:00", null, null, null, null, null, null},
+					{"15:00", null, null, null, null, null, null},
+					{"16:00", null, null, null, null, null, null},
+					{"17:00", null, null, null, null, null, null},
+					{"18:00", null, null, null, null, null, null},
+					{"19:00", null, null, null, null, null, null},
+					{"20:00", null, null, null, null, null, null},
+					{"21:00", null, null, null, null, null, null},
+				},
+				new String[] {
+					"Time", "월", "화", "수", "목", "금", "토"
+				}
+		));
+		scrolltime1.setViewportView(table_1);
+		//scrollPane_1.setSize(40, 410);
+		panel_2.setLayout(gl_panel_2);
 		
 		
 		
