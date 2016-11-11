5fa35d4693ae928156bb54ee91a1fac308d691b2
Stealth <Stealth@165.229.229.102>
2016-11-11 16:45:32 +0900

    기유 변수 수정

--- BC/src/BCAddFunc.java
+++ BC/src/BCAddFunc.java
@@ -2,17 +2,20 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.sql.Connection;
 
+import javax.swing.JTable;
 import javax.swing.JTextField;
 
 public class BCAddFunc implements ActionListener{
 	Connection con;
 	JTextField field;
+	JTable table;
 	boolean who;
-	public BCAddFunc(Connection con, JTextField field, boolean who) {
+	public BCAddFunc(Connection con,JTable table, JTextField field, boolean who) {
 		// TODO Auto-generated constructor stub
 		this.con = con;
 		this.field = field;
 		this.who = who;
+		this.table = table;
 	}
 	@Override
 	public void actionPerformed(ActionEvent e) {
