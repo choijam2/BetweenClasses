6dc22c333174236d62619afa72c376329b51c1f4
Stealth <Stealth@165.229.229.102>
2016-11-11 17:39:48 +0900

    기유 bcsearchfunc 변경

--- BC/src/BCSearchFunc.java
+++ BC/src/BCSearchFunc.java
@@ -4,13 +4,14 @@ import java.awt.event.ActionListener;
 import javax.swing.JTable;
 
 public class BCSearchFunc implements ActionListener{
-	Object o1, o2, o3, o4;
-	public BCSearchFunc(Object o1, Object o2, Object o3, Object o4) {
+	Object[][] o1, o2, o3, o4, o5;
+	public BCSearchFunc(Object[][] o1, Object[][] o2, Object[][] o3, Object[][] o4, Object[][] o5) {
 		// TODO Auto-generated constructor stub
 		this.o1 = o1;
 		this.o2 = o2;
 		this.o3 = o3;
 		this.o4 = o4;
+		this.o5 = o5;
 	}
 	@Override
 	public void actionPerformed(ActionEvent e) {
