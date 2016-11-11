656e5c0bb23dc8c024447823f9cb024a0b290557
Stealth <Stealth@st>
2016-11-11 21:34:43 +0900

    기유

--- BC/src/TimeTableSearchClassesFunc.java
+++ BC/src/TimeTableSearchClassesFunc.java
@@ -4,15 +4,18 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 
+import javax.swing.JFrame;
 import javax.swing.JTable;
 
 public class TimeTableSearchClassesFunc implements ActionListener{
-   Connection con;
+    JFrame frame;
+	Connection con;
    JTable table;
    
-   TimeTableSearchClassesFunc(Connection con,JTable table){
+   TimeTableSearchClassesFunc(Connection con,JTable table,JFrame frame){
 	   this.con = con;
 	   this.table = table;
+	   this.frame = frame;
    }
    @Override
    public void actionPerformed(ActionEvent e) {
