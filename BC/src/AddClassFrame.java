13e538e30e0d58876a51e2d0fb365c48610cebbc
choijam2 <choijam1104@gmail.com>
2016-11-20 14:23:37 +0900

    지혜/ 다시 원래대로

--- BC/src/AddClassFrame.java
+++ BC/src/AddClassFrame.java
@@ -347,7 +347,7 @@ public class AddClassFrame extends JFrame {
 		JButton FindButton = new JButton("공강찾기");
 		FindButton.setBounds(1100, 550, 97, 23);
 		panel_BCTable.add(FindButton);	
-		BCSearchFunc bsSearchBtn = new BCSearchFunc(mockTable,table_1,table_2,table_3,table_4);
+		BCSearchFunc bsSearchBtn = new BCSearchFunc(con,"21111811","21111827","-1","-1","-1");
 		FindButton.addActionListener(bsSearchBtn);
 
 		JLabel INPfNewLabel = new JLabel("성함   :");
